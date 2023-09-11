b2f2c93c010e5bfb11a2d1cb2176928056a56562
PessimistGod <rameshrahul0004@gmail.com>
2023-09-12 01:59:52 +0530

    Cors update

--- proxy-api/index.js
+++ proxy-api/index.js
@@ -13,13 +13,14 @@ const corsOptions = {
   
   app.use(cors(corsOptions));
 
-  
+
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+})
 
 const sentEmails = [];
 
